c5716eabf3bfc5f786b5f1037e5479757a2a69da
CPenalba <carolinapenalbacorpa@gmail.com>
2024-10-15 12:32:04 +0200

    first commit

---
+++ src/components/Cine.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Cine extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Cine Component</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQjn7tvtjMbIPxstW-BaXdWEoq9xWpnm1pJg&s" style={{ width: "150px", height: "150px" }}></img>
+      </div>
+    );
+  }
+}
---
+++ src/components/Musica.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Musica extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Musica Component</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6W7wWlkvcqL8UJwElSLniXBEZQaf2yNtY5Q&s" style={{ width: "150px", height: "150px" }}></img>
+      </div>
+    )
+  }
+}
